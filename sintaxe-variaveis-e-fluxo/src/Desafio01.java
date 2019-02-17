eb79c65ce4e98ce930bdebbdec52af0bdee7f10b
Silas Reis <silas.1992@live.com>
2019-02-17 16:11:18 -0300

    Desafio 01 para transformar While em For.

---
+++ sintaxe-variaveis-e-fluxo/src/Desafio01.java
@@ -0,0 +1,16 @@
+/*
+ public static void main(String args[]) {
+        int contador = 0;
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+ */
+public class Desafio01 {
+	public static void main(String[] args) {
+		for(int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+	}
+}
