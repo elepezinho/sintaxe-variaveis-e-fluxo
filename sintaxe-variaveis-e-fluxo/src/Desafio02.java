c811122a3f3813f8ad6844e5f5e549fcdf65ae03
Silas Reis <silas.1992@live.com>
2019-02-17 16:44:09 -0300

    Utilização de Break e Desafio 02

---
+++ sintaxe-variaveis-e-fluxo/src/Desafio02.java
@@ -0,0 +1,19 @@
+public class Desafio02 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * for(int multiplicador = 1; multiplicador <= 33; multiplicador++) {
+		 * System.out.println(multiplicador*3); }
+		 * 
+		 * }
+		 */
+
+		for (int i = 1; i < 100; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
